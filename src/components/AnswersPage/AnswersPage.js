ee6a8ba32bd7ac87a16fbdeef6aa83d59d8a2b3e
casscannelle <cassia.maffei@gmail.com>
2024-02-27 17:26:33 -0300

    Ajustes Form

--- src/components/AnswersPage/AnswersPage.js
+++ src/components/AnswersPage/AnswersPage.js
@@ -3,27 +3,23 @@ import { useLocation } from 'react-router-dom';
 
 const AnswersPage = () => {
   const location = useLocation();
-  const [answers, setAnswers] = useState({});
+  const [answers, setAnswers] = useState([]);
 
   useEffect(() => {
     const passedData = location.state?.answers;
     if (passedData) {
-      // Use a unique identifier as the key for each answer
-      const uniqueKey = Date.now().toString();
-      setAnswers((prevAnswers) => ({ ...prevAnswers, [uniqueKey]: passedData }));
+      setAnswers((prevAnswers) => [...prevAnswers, passedData]);
     }
   }, [location.state?.answers]);
 
   console.log('Dados do formulário depois de navegar:', answers);
-
-  const answersArray = Object.values(answers);
 
   return (
     <div className='container-wrapper'>
       <div className='container-answer'>
         <h2>Quem já se inscreveu</h2>
         <ul>
-          {answersArray.map((answer, index) => (
+          {answers.map((answer, index) => (
             <li key={index}>{`Nome: ${answer.nome}`}</li>
           ))}
         </ul>
--- src/components/FormPage/FormPage.js
+++ src/components/FormPage/FormPage.js
@@ -1,90 +1,80 @@
-import React, { useState, useEffect } from 'react';
+import { useState } from 'react';
+import React from 'react';
 import { useNavigate } from 'react-router-dom';
 
-const FormPage = () => {
-  const navigate = useNavigate();
-  const [inputValue, setInputValue] = useState({
-    nome: '',
-    email: '',
-    gatos: 'sim',
-    quantos: ''
-  });
+function FormPage() {
+  const [inputValue, setInputValue] = useState({ nome: '', email: '', gatos: 'Sim', quantos: '' });
   const [errors, setErrors] = useState({});
-  
-  
-  
+  const [responses, setResponses] = useState([]);
+  const [showAnswers, setShowAnswers] = useState(false);
+  const navigate = useNavigate();
+
   const handleChange = (event) => {
     const { name, value } = event.target;
     setInputValue((prevInputValue) => ({ ...prevInputValue, [name]: value }));
   };
-  
 
   const handleSubmit = (event) => {
     event.preventDefault();
-
+  
     const validationErrors = validateForm();
     if (Object.keys(validationErrors).length === 0) {
 
-      navigate('/answers', { state: { answers: inputValue } });
+      // Limpar o formulário
+      setInputValue({ nome: '', email: '', gatos: 'Sim', quantos: '' });
+      setErrors({});
+      alert('Enviado com sucesso')
     } else {
       setErrors(validationErrors);
     }
   };
 
-  
   const validateForm = () => {
     const errors = {};
     if (inputValue.nome.length < 3) {
-      errors.nome = 'Por favor, digite seu nome';
+      errors.nome = 'Por favor, digite um nome válido';
     }
     if (!inputValue.email.includes('@')) {
       errors.email = 'Digite um e-mail válido';
     }
-           
+
     return errors;
   };
-
-  useEffect(() => {
-    setErrors({});
-  }, [inputValue]);
-
- 
-
-  console.log('Dados do formulário antes de navegar:', inputValue);
-
 
   return (
     <div className='container-wrapper'>
       <div className='container-form'>
-        <form onSubmit={handleSubmit}>
-          <h1 className='form_h1'>Adote um gatinho</h1>
+        <form>
+          <h1 className='form_h1'>Quer adotar? <br/> Preencha seus dados</h1>
           <label>Nome:</label>
           <input className='form_input' type="text" name="nome" value={inputValue.nome} onChange={handleChange} />
           {errors.nome && <p className="error-message">{errors.nome}</p>}
-          <br/>
+
           <label>E-mail:</label>
           <input className='form_input' type="email" name="email" value={inputValue.email} onChange={handleChange} />
-          {errors.email && <p className="error-message">{errors.email}</p>}
-          <br/>
-          <label>Já tem gatos?</label>
+          {errors.nome && <p className="error-message">{errors.email}</p>}
+
+          <label>Já possui gatos?</label>
           <div>
-            <input type='radio' name="gatos" value="sim" id="sim" checked={inputValue.gatos === "sim"} onChange={handleChange} />
+            <input type='radio' name="gatos" value="Sim" id="sim" checked={inputValue.gatos === "Sim"} onChange={handleChange} />
             <label htmlFor="sim">Sim</label>
           </div>
           <div>
-            <input type='radio' name="gatos" value="nao" id="nao" checked={inputValue.gatos === "nao"} onChange={handleChange} />
+            <input type='radio' name="gatos" value="Nao" id="nao" checked={inputValue.gatos === "Nao"} onChange={handleChange} />
             <label htmlFor="nao">Não</label>
           </div>
-          <br/>
-          <label>Quantos?</label>
-          <input className='form_input' type="number" name="quantos" value={inputValue.quantos} onChange={handleChange} min="0" />
-          <br/>
-          <button className='btn' type="submit" >Enviar</button>
-          
+          <div>
+            <label>Quantos?</label>
+            <input className='form_input' type="number" name="quantos" value={inputValue.quantos} onChange={handleChange} min="0" />
+          </div>
+
+          <button className='btn' type="submit" onClick={handleSubmit}>
+            Enviar
+          </button>
         </form>
       </div>
     </div>
   );
-};
+}
 
 export default FormPage;
